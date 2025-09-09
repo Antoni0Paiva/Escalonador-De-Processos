0f36512076e3cc78959ae65f32b810ab3b1b1a8c
Antoni0Paiva <antonioahoy@gmail.com>
2025-09-08 21:11:02 -0300

    Construtor Classe Processo

--- src/escalonador/Processo.java
+++ src/escalonador/Processo.java
@@ -8,3 +8,10 @@ int prioridade;
 int ciclos_necessarios;
 String recurso_necessario;
 
+public Processo (int id, String nome, int prioridade, int ciclos_necessarios, String recurso_necessario) {
+    this.id = id;
+    this.nome = nome;
+    this.prioridade = prioridade;
+    this.ciclos_necessarios = ciclos_necessarios;
+    this. recurso_necessario = recurso_necessario;
+}
