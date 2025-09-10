dc7196e3e56352b5779d1ec14a4b982803c041e9
Antoni0Paiva <antonioahoy@gmail.com>
2025-09-10 03:21:30 -0300

    Adição do método adicionar na Classe ListaDeProcessos

--- src/escalonador/ListaDeProcessos.java
+++ src/escalonador/ListaDeProcessos.java
@@ -8,4 +8,15 @@ public class ListaDeProcessos {
         this.inicio = null;
         this.fim = null;
     }
+    public void adicionar(Processo processo) {
+        No novo = new No(processo);
+        if (inicio == null) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            fim.proximo = novo;
+            fim = novo;
+        }
+    }
+    
 }
