ca02371af3e1f9c5417faf350dff4bb0c69fb0f3
Antoni0Paiva <antonioahoy@gmail.com>
2025-09-14 21:25:23 -0300

    Implementa construtor da classe Scheduler

--- src/escalonador/Scheduler.java
+++ src/escalonador/Scheduler.java
@@ -7,4 +7,12 @@ public class Scheduler {
     private ListaDeProcessos listaBloqueados;
     private int contador_ciclos_alta_prioridade;
 
+    public Scheduler() {
+        this.listaAltaPrioridade = new ListaDeProcessos();
+        this.listaMediaPrioridade = new ListaDeProcessos();
+        this.listaBaixaPrioridade = new ListaDeProcessos();
+        this.listaBloqueados = new ListaDeProcessos();
+        this.contador_ciclos_alta_prioridade = 0;
+    }
+
 }
