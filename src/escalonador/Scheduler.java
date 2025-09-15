d9fadebad0af6c5305706419fa84c1aa362b939e
Antoni0Paiva <antonioahoy@gmail.com>
2025-09-14 21:21:26 -0300

    Adiciona estrutura inicial e atributos da classe Scheduler

--- src/escalonador/Scheduler.java
+++ src/escalonador/Scheduler.java
@@ -1,4 +1,10 @@
 package escalonador;
 
 public class Scheduler {
+    private ListaDeProcessos listaAltaPrioridade;
+    private ListaDeProcessos listaMediaPrioridade;
+    private ListaDeProcessos listaBaixaPrioridade;
+    private ListaDeProcessos listaBloqueados;
+    private int contador_ciclos_alta_prioridade;
+
 }
