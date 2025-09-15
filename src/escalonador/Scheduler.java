0dee2f526e5b945da0684969b45894d83ab80ce2
Antoni0Paiva <antonioahoy@gmail.com>
2025-09-14 21:30:01 -0300

    Renomeia atributos da classe Schedulerr

--- src/escalonador/Scheduler.java
+++ src/escalonador/Scheduler.java
@@ -1,17 +1,17 @@
 package escalonador;
 
 public class Scheduler {
-    private ListaDeProcessos listaAltaPrioridade;
-    private ListaDeProcessos listaMediaPrioridade;
-    private ListaDeProcessos listaBaixaPrioridade;
-    private ListaDeProcessos listaBloqueados;
+    private ListaDeProcessos lista_alta_prioridade;
+    private ListaDeProcessos lista_media_prioridade;
+    private ListaDeProcessos lista_baixa_prioridade;
+    private ListaDeProcessos lista_bloqueados;
     private int contador_ciclos_alta_prioridade;
 
     public Scheduler() {
-        this.listaAltaPrioridade = new ListaDeProcessos();
-        this.listaMediaPrioridade = new ListaDeProcessos();
-        this.listaBaixaPrioridade = new ListaDeProcessos();
-        this.listaBloqueados = new ListaDeProcessos();
+        this.lista_alta_prioridade = new ListaDeProcessos();
+        this.lista_media_prioridade = new ListaDeProcessos();
+        this.lista_baixa_prioridade = new ListaDeProcessos();
+        this.lista_bloqueados = new ListaDeProcessos();
         this.contador_ciclos_alta_prioridade = 0;
     }
 
