0350b52fdf85576033af7ae2be67c76aae9fe6d5
Antoni0Paiva <antonioahoy@gmail.com>
2025-09-17 01:13:28 -0300

    Adiciona a configuração inicial da classe Main e verificação de argumento

--- src/escalonador/Main.java
+++ src/escalonador/Main.java
@@ -1,7 +1,13 @@
 package escalonador;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Escalonador iniciado!");
+        if (args.length < 1) {
+            System.out.println("Uso: java escalonador.Main <arquivo.csv>");
+            return;
+        }
     }
 }
